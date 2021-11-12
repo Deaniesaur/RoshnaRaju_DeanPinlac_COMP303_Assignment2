dd511dddd3a782e3bd685e8e90c6ac7f6e1019b3
Dean Pinlac <dean.pinlac@gmail.com>
2021-11-12 16:27:55 -0500

    Add Confirmation Page

--- src/main/java/com/comp303/lab2/Controllers/AuthController.java
+++ src/main/java/com/comp303/lab2/Controllers/AuthController.java
@@ -1,7 +1,5 @@
 package com.comp303.lab2.Controllers;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
--- src/main/java/com/comp303/lab2/Models/Enrollment.java
+++ src/main/java/com/comp303/lab2/Models/Enrollment.java
@@ -11,9 +11,11 @@ import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Enrollment {
 	
